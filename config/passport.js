03e44b16f7319409f27864253c1de396b259660e
Prakash Palsaniya <150008749+PrakashPalsaniya@users.noreply.github.com>
2025-10-20 11:31:27 +0530

    sfs

--- config/passport.js
+++ config/passport.js
@@ -5,7 +5,7 @@ const User = require('../models/User');
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-     callbackURL:  "http://localhost:5000/api/v1/auth/google/callback" //process.env.GOOGLE_CALLBACK_URL ||//
+     callbackURL:  process.env.GOOGLE_CALLBACK_URL
   },
   async (accessToken, refreshToken, profile, done) => {
     try {
